1d84d4725800bcfc157ea90be01d90bdce1a02c2
yoitsmedarls <johndaryljavier26@gmail.com>
2025-02-11 13:44:43 +0800

    Moved the courses navigation bar inside the courses route

---
+++ src/routes/courses/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte';
+  import type { LayoutData } from './$types';
+
+  let { data, children }: { data: LayoutData; children: Snippet } = $props();
+</script>
+
+<nav
+  class="sticky top-0 flex h-auto w-auto flex-nowrap justify-end gap-x-2 bg-slate-200 p-2 align-middle"
+>
+  <a
+    class="w-25 flex flex-row justify-center rounded-md bg-slate-300 px-4 py-4 align-middle hover:bg-slate-400 active:bg-slate-500"
+    href="/courses"
+  >
+    All Courses
+  </a>
+</nav>
+
+{@render children()}
