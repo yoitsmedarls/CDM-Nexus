d719d670232189295884a6912078c601c45975ca
yoitsmedarls <johndaryljavier26@gmail.com>
2025-02-14 22:41:05 +0800

    Indicated entity relations as a comment

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -4,3 +4,11 @@ export * from './schema/lessons';
 export * from './schema/questions';
 export * from './schema/quizzes';
 export * from './schema/topics';
+
+/**
+ * Courses - Lessons          : 1-*
+ * Lessons - Topics           : 1-*
+ * Topics - LectureMaterials  : 1-1
+ * Topics - Quizzes           : 1-1
+ * Quizzes - Questions        : 1-*
+ */
