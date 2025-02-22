0a96cddfa9570c3d7e311c89bce715f004d6b3d0
yoitsmedarls <johndaryljavier26@gmail.com>
2025-02-22 20:07:05 +0800

    Finalized users and sessions schema.

--- src/lib/server/db/schema/admin/users.ts
+++ src/lib/server/db/schema/admin/users.ts
@@ -1,20 +1,39 @@
 import { sql } from 'drizzle-orm';
-import { check, pgTable, text, uuid, varchar } from 'drizzle-orm/pg-core';
+import {
+  check,
+  pgEnum,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+  varchar,
+} from 'drizzle-orm/pg-core';
+
+export const roleEnum = pgEnum('role', ['admin', 'tutor']);
 
 export const users = pgTable(
   'users',
   {
-    id: uuid('id').defaultRandom().notNull(),
-    username: varchar('username', { length: 30 }).primaryKey(),
-    firstName: varchar('first_name', { length: 50 }).notNull(),
-    lastName: varchar('last_name', { length: 50 }).notNull(),
-    email: varchar('email', { length: 320 }).unique().notNull(),
+    id: uuid('id').primaryKey().notNull(),
+    username: varchar('username', { length: 30 }).unique().notNull(),
+    fullName: varchar('full_name', { length: 255 }).notNull(),
+    cdm_email: varchar('cdm_email', { length: 320 }).unique().notNull(),
+    role: roleEnum('role').default('tutor').notNull(),
     passwordHash: text('password_hash').notNull(),
+    dateJoined: timestamp('date_joined', {
+      withTimezone: true,
+    })
+      .defaultNow()
+      .notNull(),
   },
   (table) => [
     check(
       'check_username_format',
       sql`${table.username} ~ '^[A-Za-z][A-Za-z0-9_]{5,30}$'`
+    ),
+    check(
+      'check_cdm_email_format',
+      sql`${table.cdm_email} ~ '^[A-za-z0-9.]+(@cdm.edu.ph)$'`
     ),
   ]
 );
