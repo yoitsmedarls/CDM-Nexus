cbff325ad56913737f43ad23926b452682d67b8f
yoitsmedarls <johndaryljavier26@gmail.com>
2025-02-23 21:46:52 +0800

    Initial installation of Lucia Auth.

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,3 +1,4 @@
+import { pgTable, text, timestamp } from 'drizzle-orm/pg-core';
 // Exports for courses-related tables
 export * from './schema/courses/stateEnum';
 export * from './schema/courses/termEnum';
@@ -9,5 +10,26 @@ export * from './schema/courses/quiz';
 export * from './schema/courses/question';
 
 // Exports for admin-related tables
-// export * from './schema/admin/user';
-// export * from './schema/admin/session';
+export * from './schema/admin/user';
+export * from './schema/admin/session';
+
+export const user = pgTable('user', {
+  id: text('id').primaryKey(),
+  username: text('username').notNull().unique(),
+  passwordHash: text('password_hash').notNull(),
+});
+
+export const session = pgTable('session', {
+  id: text('id').primaryKey(),
+  userId: text('user_id')
+    .notNull()
+    .references(() => user.id),
+  expiresAt: timestamp('expires_at', {
+    withTimezone: true,
+    mode: 'date',
+  }).notNull(),
+});
+
+export type Session = typeof session.$inferSelect;
+
+export type User = typeof user.$inferSelect;
