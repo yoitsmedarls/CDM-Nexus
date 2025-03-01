9ff53f300bece86726ba91152b1567a401d80aff
John Daryl Ugaban <johndaryljavier26@gmail.com>
2025-03-01 23:07:29 +0800

    Changed the adapter. (#34)

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,16 +12,8 @@ const config = {
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter({
-      routes: {
-        include: ['/*'],
-        exclude: ['<all>'],
-      },
-      platformProxy: {
-        configPath: 'wrangler.toml',
-        environment: undefined,
-        experimentalJsonConfig: false,
-        persist: false,
-      },
+      edge: false,
+      split: true,
     }),
 
     csrf: {
