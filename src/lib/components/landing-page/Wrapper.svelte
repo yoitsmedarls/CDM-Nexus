55d107cf41660bde54b204eec3f49185a948824e
yoitsmedarls <johndaryljavier26@gmail.com>
2025-03-04 06:11:30 +0800

    Removed the bottom divider on wrapper.

--- src/lib/components/landing-page/Wrapper.svelte
+++ src/lib/components/landing-page/Wrapper.svelte
@@ -18,27 +18,12 @@
   {id}
   class={['flex min-h-fit flex-col px-4', { limitmaxheight }, { heightsvh }]}
 >
-  <div
-    class="bottom-border-divider mx-auto flex w-full max-w-[88rem] grow flex-col"
-  >
+  <div class={['mx-auto flex w-full max-w-[88rem] grow flex-col']}>
     {@render children()}
   </div>
 </section>
 
 <style lang="postcss">
-  .bottom-border-divider {
-    border-bottom: 2px solid;
-    border-image: linear-gradient(
-        to right,
-        transparent 25%,
-        #06266510 40%,
-        #06266520 50%,
-        #06266510 60%,
-        transparent 75%
-      )
-      100% 1;
-    border-image-outset: 2rem;
-  }
   .limitmaxheight {
     max-height: 88rem;
   }
