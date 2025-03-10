d218c33c85ccd4fc32b59bf0571af0e3a89203f3
yoitsmedarls <johndaryljavier26@gmail.com>
2025-03-10 20:07:26 +0800

    Added a role check to prevent non-admin users from accessing the admin route.

--- src/routes/admin/+page.server.ts
+++ src/routes/admin/+page.server.ts
@@ -2,8 +2,13 @@ import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async (event) => {
-  if (!event.locals.user) {
+  if (event.locals.user === null) {
     return redirect(302, '/admin/login');
   }
+
+  if (event.locals.user.role !== 'admin') {
+    return redirect(302, '/admin/login');
+  }
+
   return redirect(302, '/admin/dashboard');
 };
