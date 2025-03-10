357e17738e2f6aab3b8ed3ede9b8232e873c3835
yoitsmedarls <johndaryljavier26@gmail.com>
2025-03-10 23:02:44 +0800

    Clean start.

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,5 @@
-// See https://svelte.dev/docs/kit/types#app.d.ts
-// for information about these interfaces
 declare global {
-  namespace App {
-    interface Locals {
-      user: import('$lib/server/auth').SessionValidationResult['user'];
-      session: import('$lib/server/auth').SessionValidationResult['session'];
-    }
-  }
+  namespace App {}
 }
 
 export {};
--- src/routes/courses/[slug]/lessons/[slug]/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  import type { PageData } from './$types';
-
-  let { data }: { data: PageData } = $props();
-</script>
-
-<h1 class="py-5 pl-2 text-4xl font-bold">{data.queriedLesson?.title}</h1>
-<p class="pl-2 text-xl">{data.queriedLesson?.description}</p>
