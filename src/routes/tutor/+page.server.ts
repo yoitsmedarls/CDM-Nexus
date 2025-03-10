39db60c98dbc23b411656cf4cfc8ff901d546abd
yoitsmedarls <johndaryljavier26@gmail.com>
2025-03-11 02:59:31 +0800

    Removed redundant route protections given that the hook already handles it.

--- src/routes/tutor/+page.server.ts
+++ src/routes/tutor/+page.server.ts
@@ -1,15 +1,6 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
-import { handleLoginRedirect } from '$lib/server/auth/utils';
 
-export const load = (async (event) => {
-  if (
-    !event.locals.session ||
-    !event.locals.user ||
-    event.locals.user.role !== 'tutor'
-  ) {
-    return redirect(302, handleLoginRedirect(event));
-  }
-
+export const load = (async () => {
   return redirect(303, '/tutor/home');
 }) satisfies PageServerLoad;
