c7786a3f3908f51e9be14579bb1ee8449ef957ae
yoitsmedarls <johndaryljavier26@gmail.com>
2025-03-12 00:28:46 +0800

    Moved the border to the main wrapper.

--- src/lib/components/admin/dashboard/main/DashboardBody.svelte
+++ src/lib/components/admin/dashboard/main/DashboardBody.svelte
@@ -4,9 +4,7 @@
   let { children }: { children?: Snippet } = $props();
 </script>
 
-<div
-  class="flex grow flex-col rounded-b-md border-gray-200 p-2 xl:border-x-1 xl:border-b-1"
->
+<div class="flex grow flex-col rounded-b-md p-2">
   {#if children}
     {@render children()}
   {/if}
