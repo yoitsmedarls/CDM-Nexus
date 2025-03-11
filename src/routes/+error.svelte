e569c6bbc982d01795dbfb5a25043e95083eb4db
yoitsmedarls <johndaryljavier26@gmail.com>
2025-03-12 02:58:47 +0800

    Added a stylized error page.

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { page } from '$app/state';
+</script>
+
+<main class="flex grow flex-col justify-center">
+  <h1
+    class="font-poppins 2xs:text-xl text-cdm-red-600 p-4 text-center text-lg font-semibold text-balance sm:text-2xl lg:text-3xl"
+  >
+    Error {page.status}:
+    <br />
+    {page.error?.message}
+  </h1>
+</main>
