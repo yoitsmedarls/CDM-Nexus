c6825ea7214a96da86ba9e067e3b685102051839
yoitsmedarls <johndaryljavier26@gmail.com>
2025-03-12 01:02:37 +0800

    Commented out currently unused variables and imports to get rid of eslint errors.

--- src/routes/(app)/home/+page.svelte
+++ src/routes/(app)/home/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-  import type { PageData } from './$types';
+  // import type { PageData } from './$types';
 
-  let { data }: { data: PageData } = $props();
+  // let { data }: { data: PageData } = $props();
 </script>
 
 <h1>Home</h1>
