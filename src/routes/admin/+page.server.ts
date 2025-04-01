3d9e84460b9ebaa3a6e978e853f1f11eb409ed93
yoitsmedarls <johndaryljavier26@gmail.com>
2025-04-02 02:48:41 +0800

    Moved the logout action to the root admin route.

--- src/routes/admin/+page.server.ts
+++ src/routes/admin/+page.server.ts
@@ -1,6 +1,24 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
+import type { Actions } from '@sveltejs/kit';
+import { fail } from '@sveltejs/kit';
+import { invalidateSession } from '$lib/server/auth/session';
+import { deleteSessionTokenCookie } from '$lib/server/auth/session';
 
 export const load = (async () => {
   return redirect(303, '/admin/dashboard');
 }) satisfies PageServerLoad;
+
+export const actions: Actions = {
+  logout: async (event) => {
+    if (!event.locals.session) {
+      return fail(401);
+    }
+    await invalidateSession(event.locals.session.id);
+    deleteSessionTokenCookie(event);
+
+    console.log('User logged out.');
+
+    return redirect(302, '/login');
+  },
+};
