3acdbd9a261f235f4b30cacd39beeb5252f8ac56
yoitsmedarls <johndaryljavier26@gmail.com>
2025-04-02 02:48:41 +0800

    Separated various parts of the dashboard into their own components.

---
+++ src/lib/components/admin/MainWrapper.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte';
+
+  let { children }: { children?: Snippet } = $props();
+</script>
+
+<main
+  class="flex w-full grow flex-col overflow-clip transition-all duration-200"
+>
+  {#if children}
+    {@render children()}
+  {/if}
+</main>
---
+++ src/lib/components/admin/dashboard/sidepanel/CalendarWidget.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+</script>
+
+<section
+  class="min-h-48 rounded-md border-gray-200 bg-white p-2 max-md:bg-gray-100"
+>
+  <h2
+    class="font-poppins 2xs:text-[0.875rem] pb-2 text-[0.75rem] font-semibold text-gray-800 transition-all duration-100 sm:text-base lg:text-lg"
+  >
+    Calendar
+  </h2>
+  <div id="calendar"></div>
+</section>
+
+<style>
+</style>
