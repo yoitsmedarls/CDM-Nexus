71361f84962728432d7908a5e397c3466f887378
yoitsmedarls <johndaryljavier26@gmail.com>
2025-04-02 03:58:20 +0800

    Removed unnecessary Tailwind plugin.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,19 @@
-<h1>Hello, world!</h1>
+<article class="prose">
+  <h1>Hello, world!</h1>
+  <p>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+    obcaecati, laboriosam animi, eum est ex vel veniam eos veritatis reiciendis
+    eligendi deserunt ad qui odit? Corporis dicta alias distinctio aperiam?
+  </p>
+</article>
+
+<form>
+  <label class="block">
+    <span>Name</span>
+    <input type="text" name="name" id="name" />
+  </label>
+  <label class="block">
+    <span>Email</span>
+    <input type="email" name="email" id="email" />
+  </label>
+</form>
