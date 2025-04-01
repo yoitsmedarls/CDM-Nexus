faa6666f2b18550bf7bc74a717f4a12e39d966b1
yoitsmedarls <johndaryljavier26@gmail.com>
2025-04-02 02:48:41 +0800

    Added an optional padding to the main wrapper.

--- src/lib/components/admin/MainWrapper.svelte
+++ src/lib/components/admin/MainWrapper.svelte
@@ -1,13 +1,25 @@
 <script lang="ts">
   import type { Snippet } from 'svelte';
 
-  let { children }: { children?: Snippet } = $props();
+  let {
+    children,
+    addPadding = false,
+  }: { children?: Snippet; addPadding?: boolean } = $props();
 </script>
 
 <main
-  class="flex w-full grow flex-col overflow-clip rounded-md border-gray-200 transition-all duration-200 xl:border-1"
+  class={[
+    'flex w-full grow flex-col overflow-clip rounded-md border-gray-200 transition-all duration-200 xl:border-1',
+    { addPadding },
+  ]}
 >
   {#if children}
     {@render children()}
   {/if}
 </main>
+
+<style>
+  .addPadding {
+    padding: 0.5rem;
+  }
+</style>
