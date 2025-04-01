4b3e459ccfeff5221105e4436ab1b3be4b93ebd3
yoitsmedarls <johndaryljavier26@gmail.com>
2025-04-02 02:48:41 +0800

    Fixed falsity checks.

--- src/routes/hooks.server.ts
+++ src/routes/hooks.server.ts
@@ -9,7 +9,7 @@ import type { Handle } from '@sveltejs/kit';
 const authHandler: Handle = async ({ event, resolve }) => {
   const sessionToken = event.cookies.get(sessionCookieName) ?? null;
 
-  if (sessionToken === null) {
+  if (!sessionToken) {
     event.locals.user = null;
     event.locals.session = null;
     return resolve(event);
--- src/routes/tutor/+page.server.ts
+++ src/routes/tutor/+page.server.ts
@@ -4,8 +4,8 @@ import { handleLoginRedirect } from '$lib/server/auth/utils';
 
 export const load = (async (event) => {
   if (
-    event.locals.session === null ||
-    event.locals.user === null ||
+    !event.locals.session ||
+    !event.locals.user ||
     event.locals.user.role !== 'tutor'
   ) {
     return redirect(302, handleLoginRedirect(event));
