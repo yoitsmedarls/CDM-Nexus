375d60e8c0e8790d5e66dd8587bb7199f0b9a66a
yoitsmedarls <johndaryljavier26@gmail.com>
2025-04-02 02:48:41 +0800

    Clean start.

--- src/routes/admin/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types';
-
-export const load: PageServerLoad = async (event) => {
-  if (!event.locals.user) {
-    return redirect(302, '/admin/login');
-  }
-  return redirect(302, '/admin/dashboard');
-};
--- src/routes/course-list/+page.svelte
+++
@@ -1,3 +0,0 @@
-<h1 class="font-merriweather text-italic py-3 text-3xl font-bold text-cyan-950">
-  Welcome!
-</h1>
