dfd6215b4adc57cf12b802c5ebed36f546a8f0bf
yoitsmedarls <johndaryljavier26@gmail.com>
2025-04-02 02:48:41 +0800

    Re-added the dashboard page and controlled the data sent to the client.

--- src/routes/admin/dashboard/+page.server.ts
+++ src/routes/admin/dashboard/+page.server.ts
@@ -1,5 +1,27 @@
-import type { PageServerLoad } from './$types';
+import { fail, redirect } from '@sveltejs/kit';
+import type { Actions, PageServerLoad } from './$types';
+import { invalidateSession } from '$lib/server/auth/session';
+import { deleteSessionTokenCookie } from '$lib/server/auth/session';
 
-export const load = (async () => {
-  return {};
-}) satisfies PageServerLoad;
+export const load: PageServerLoad = async (event) => {
+  const { ...user } = event.locals.user;
+
+  return {
+    user: {
+      username: user.username,
+      cdmEmail: user.cdmEmail,
+    },
+  };
+};
+
+export const actions: Actions = {
+  default: async (event) => {
+    if (!event.locals.session) {
+      return fail(401);
+    }
+    await invalidateSession(event.locals.session.id);
+    deleteSessionTokenCookie(event);
+
+    return redirect(302, '/login');
+  },
+};
