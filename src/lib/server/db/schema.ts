ad33cd9604b68ca40f59455e4fc764949dc34ddf
yoitsmedarls <johndaryljavier26@gmail.com>
2025-04-04 05:06:36 +0800

    Initiated the database connection and compiled the schemas.

---
+++ src/lib/server/db/schema.ts
@@ -0,0 +1,17 @@
+// Exports for courses-related tables
+export * from './schema/stateEnum';
+export * from './schema/termEnum';
+export * from './schema/courses';
+export * from './schema/lessons';
+export * from './schema/topics';
+export * from './schema/lectureMaterials';
+export * from './schema/quizzes';
+export * from './schema/quizQuestions';
+export * from './schema/exams';
+export * from './schema/examQuestions';
+
+// Exports for user-related tables
+export * from './schema/userRoleEnum';
+export * from './schema/users';
+export * from './schema/sessions';
+export * from './schema/passwordResetSessions';
