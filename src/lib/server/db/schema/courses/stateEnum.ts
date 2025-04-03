8849fbd5258fa742084e2da43484975aab21aa51
John Daryl Ugaban <johndaryljavier26@gmail.com>
2025-04-04 04:51:01 +0800

    Reset/branch-cleanup (#83)
    * Clean slate.
    
    * Further cleanup.
    
    * Removed unnecessary Tailwind plugin.
    
    * Added the CSS Theme.
    
    * Added descriptive comments to the main stylesheet.
    
    * Final dependency update before merging.

--- src/lib/server/db/schema/courses/stateEnum.ts
+++
@@ -1,3 +0,0 @@
-import { pgEnum } from 'drizzle-orm/pg-core';
-
-export const stateEnum = pgEnum('status', ['ongoing', 'complete']);
--- src/lib/server/db/schema/courses/termEnum.ts
+++
@@ -1,3 +0,0 @@
-import { pgEnum } from 'drizzle-orm/pg-core';
-
-export const termEnum = pgEnum('term', ['midterm', 'finals']);
