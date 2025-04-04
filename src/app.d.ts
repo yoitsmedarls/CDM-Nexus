5e0b348b646ffc90bcde1acf7607e00a473657e7
yoitsmedarls <johndaryljavier26@gmail.com>
2025-04-04 13:28:20 +0800

    Added hooks and types for TS.

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,12 @@
+import type { SessionValidationResult } from '$lib/server/auth/session';
+
 declare global {
-  namespace App {}
+  namespace App {
+    interface Locals {
+      user: SessionValidationResult['user'];
+      session: SessionValidationResult['session'];
+    }
+  }
 }
 
 export {};
