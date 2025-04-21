62181b4f02e952f498df3e62dc491d30dd0eedf8
yoitsmedarls <johndaryljavier26@gmail.com>
2025-04-21 14:40:13 +0800

    Renaming the handlers in the server hooks to improve readability.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -8,13 +8,13 @@ import { loginRedirect, roleBasedRedirect } from '$lib/server/auth/utils';
 import { redirect, type Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
 
-const authHandler: Handle = async ({ event, resolve }) => {
+const authenticationHandler: Handle = async ({ event, resolve }) => {
   const sessionToken = event.cookies.get(sessionCookieName) ?? null;
 
   if (!sessionToken) {
     event.locals.user = null;
     event.locals.session = null;
-    console.log('Auth Handler: No session token found.');
+    console.log('Authentication Handler: No session token found.');
     return resolve(event);
   }
 
@@ -22,10 +22,14 @@ const authHandler: Handle = async ({ event, resolve }) => {
 
   if (session) {
     setSessionTokenCookie(event, sessionToken, session.expiresAt);
-    console.log(`Auth Handler: Session valid for user: ${user?.username}`);
+    console.log(
+      `Authentication Handler: Session valid for user: ${user?.username}`
+    );
   } else {
     deleteSessionTokenCookie(event);
-    console.log('Auth Handler: Invalid session token. Cleared cookie.');
+    console.log(
+      'Authentication Handler: Invalid session token. Cleared cookie.'
+    );
   }
 
   event.locals.user = user;
@@ -34,7 +38,7 @@ const authHandler: Handle = async ({ event, resolve }) => {
   return resolve(event);
 };
 
-const protectedRoutesHandler: Handle = async ({ event, resolve }) => {
+const authorizationHandler: Handle = async ({ event, resolve }) => {
   const protectedRoutes = [
     {
       role: 'admin',
@@ -62,7 +66,7 @@ const protectedRoutesHandler: Handle = async ({ event, resolve }) => {
   if (isProtectedRoute) {
     if (!user || !session) {
       console.log(
-        'Protected Routes Handler: User tried to access',
+        'Authorization Handler: User tried to access',
         currentPath,
         'without an account.'
       );
@@ -75,28 +79,29 @@ const protectedRoutesHandler: Handle = async ({ event, resolve }) => {
 
     if (matchedRoute && user.role !== matchedRoute.role) {
       console.log(
-        `Protected Routes Handler: User with role '${user.role}' tried to access ${currentPath} (requires '${matchedRoute.role}').`
+        `Authorization Handler: User with role '${user.role}' tried to access ${currentPath} (requires '${matchedRoute.role}').`
       );
       throw redirect(302, roleBasedRedirect(user.role));
     }
 
     console.log(
-      `Protected Routes Handler: User with role '${user.role}' accessing ${currentPath}`
+      `Authorization Handler: User with role '${user.role}' accessing ${currentPath}`
     );
   }
   if (isAuthRoute) {
     if (user && session) {
       console.log(
-        `Protected Routes Handler: Logged-in user tried to access ${currentPath}. Redirecting.`
+        `Authorization Handler: Logged-in user tried to access ${currentPath}. Redirecting.`
       );
       throw redirect(302, roleBasedRedirect(user.role));
     }
-    console.log(
-      `Protected Routes Handler: Guest user accessing ${currentPath}.`
-    );
+    console.log(`Authorization Handler: Guest user accessing ${currentPath}.`);
   }
 
   return resolve(event);
 };
 
-export const handle: Handle = sequence(authHandler, protectedRoutesHandler);
+export const handle: Handle = sequence(
+  authenticationHandler,
+  authorizationHandler
+);
