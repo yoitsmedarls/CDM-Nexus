3cb1a24dc9eec225a78e90a1e592ebe0adaed20e
yoitsmedarls <johndaryljavier26@gmail.com>
2025-04-22 23:44:01 +0800

    Adding a global loading screen to inform users of loading states.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
+  import { navigating } from '$app/state';
+  import LoadingScreen from '$lib/components/global/LoadingScreen.svelte';
+  import { blur } from 'svelte/transition';
   import '../app.css';
   import type { Snippet } from 'svelte';
+  import { cubicInOut } from 'svelte/easing';
 
   let { children }: { children: Snippet } = $props();
 </script>
+
+{#await navigating.complete}
+  <LoadingScreen />
+{/await}
 
 {@render children()}
