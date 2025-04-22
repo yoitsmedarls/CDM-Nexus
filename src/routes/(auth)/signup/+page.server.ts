a2e3c7c5d9e64b9b7346807253716dbae25ce729
John Daryl Ugaban <johndaryljavier26@gmail.com>
2025-04-22 23:51:14 +0800

    Refactoring Auth Routes/API and adding a global loading indicator. (#119)
    * Refactored and optimized the auth routes program logic.
    
    * Several modifications to the Auth API following the changes to the auth routes.
    
    * Adding a global loading screen to inform users of loading states.

--- src/routes/(auth)/signup/+page.server.ts
+++ src/routes/(auth)/signup/+page.server.ts
@@ -1,27 +1,22 @@
 import { fail, redirect } from '@sveltejs/kit';
-import type { Actions, PageServerLoad } from './$types';
+import type { Actions } from './$types';
 import {
-  checkUsernameAvailability,
   createUser,
+  getUserByCdmEmail,
+  getUserByUsername,
+  verifyCDMEmailInput,
   verifyFullNameInput,
   verifyUsernameInput,
 } from '$lib/server/auth/user';
 import { verifyPasswordStrength } from '$lib/server/auth/password';
 import { generateSessionToken, generateUserId } from '$lib/server/auth/utils';
 import { createSession, setSessionTokenCookie } from '$lib/server/auth/session';
-import {
-  checkEmailAvailability,
-  verifyEmailInput,
-} from '$lib/server/auth/email';
-
-export const load = (async (event) => {
-  return { redirectTo: event.url.search };
-}) satisfies PageServerLoad;
 
 export const actions: Actions = {
   default: async (event) => {
     const formData: FormData = await event.request.formData();
 
+    // Getting all inputs from the user
     const fullName: string | undefined = formData
       .get('fullname')
       ?.toString()
@@ -39,42 +34,57 @@ export const actions: Actions = {
       ?.toString()
       .trim();
 
+    // Checks if any of the required fields are empty.
+    // Prompts the user to fill up empty fields.
     if (!username || !password || !fullName || !cdmEmail) {
       return fail(400, { message: 'Fill in the required fields.' });
     }
+
+    // Ensures a proper full name was entered
+    // (at least 5 characters and one space).
     if (!verifyFullNameInput(fullName)) {
       return fail(400, {
         message: 'Please enter your full name.',
       });
     }
-    if (!verifyEmailInput(cdmEmail)) {
+
+    // Validates the CDM Email format through RegEx
+    if (!verifyCDMEmailInput(cdmEmail)) {
       return fail(400, {
         message: 'Invalid email. Use your CDM email only.',
       });
     }
+
+    // Validates the username input through length checks and RegEx.
     if (!verifyUsernameInput(username)) {
       return fail(400, {
         message:
           'Invalid username. Use alphanumeric characters only (min: 5, max: 30).',
       });
     }
+
+    // Checks password length and compared against list of pwned passwords.
     if (!(await verifyPasswordStrength(password))) {
       return fail(400, {
         message:
           'Password too weak. Use characters, numbers, and symbols (min: 8, max: 255).',
       });
     }
-    if (!(await checkUsernameAvailability(username))) {
+
+    // Queries the database twice, checking for either an
+    //  existing user with the given username or CDM email.
+    if (await getUserByUsername(username)) {
       return fail(400, {
         message: 'An account already exists with the username provided',
       });
     }
-    if (!(await checkEmailAvailability(cdmEmail))) {
+    if (await getUserByCdmEmail(cdmEmail)) {
       return fail(400, {
         message: 'An account already exists with the email provided',
       });
     }
 
+    // All validation checks completed, proceeding to account creation
     try {
       const createdUser = await createUser(
         generateUserId(),
@@ -84,6 +94,7 @@ export const actions: Actions = {
         password
       );
 
+      // Setting up session tokens.
       const sessionToken = generateSessionToken();
       const session = await createSession(sessionToken, createdUser.id);
       setSessionTokenCookie(event, sessionToken, session.expiresAt);
@@ -96,6 +107,10 @@ export const actions: Actions = {
       });
     }
 
+    // Checking if URL has redirect targets.
+    // If so, redirect user to such target.
+    // Access to certain pages are still managed by the server hooks,
+    // so unauthorized access is not of concern.
     if (event.url.searchParams.has('redirectTo')) {
       const redirectTo = event.url.searchParams.get('redirectTo');
 
@@ -104,6 +119,7 @@ export const actions: Actions = {
       }
     }
 
+    // Returns a success prompt the user.
     return { success: true };
   },
 };
