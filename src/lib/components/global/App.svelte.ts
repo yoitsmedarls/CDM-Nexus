2590b7f11c0ebb174c05bff2da21c4dc3eb7411a
yoitsmedarls <johndaryljavier26@gmail.com>
2025-04-28 01:32:04 +0800

    Adding a globally accessible viewport width and height value.

---
+++ src/lib/components/global/App.svelte.ts
@@ -0,0 +1,4 @@
+export const App = $state({
+  viewportWidth: 0,
+  viewportHeight: 0,
+});
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,15 @@
   import type { Snippet } from 'svelte';
   import { navigating } from '$app/state';
   import LoadingScreen from '$lib/components/global/LoadingScreen.svelte';
+  import { App } from '$lib/components/global/App.svelte';
 
   let { children }: { children: Snippet } = $props();
 </script>
+
+<svelte:window
+  bind:innerWidth={App.viewportWidth}
+  bind:innerHeight={App.viewportHeight}
+/>
 
 {#await navigating.complete}
   <LoadingScreen />
