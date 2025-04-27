08fa463ab1434bd85a9b84131fcf267a4c6268f5
yoitsmedarls <johndaryljavier26@gmail.com>
2025-04-28 01:08:14 +0800

    Removing unused imports for now.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
   import { navigating } from '$app/state';
   import LoadingScreen from '$lib/components/global/LoadingScreen.svelte';
-  import { blur } from 'svelte/transition';
   import '../app.css';
   import type { Snippet } from 'svelte';
-  import { cubicInOut } from 'svelte/easing';
 
   let { children }: { children: Snippet } = $props();
 </script>
