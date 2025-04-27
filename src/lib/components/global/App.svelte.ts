3929516b8a033afe89450aff4451bad1dcc0891d
yoitsmedarls <johndaryljavier26@gmail.com>
2025-04-28 01:44:56 +0800

    Adding a global breakpoints object.

--- src/lib/components/global/App.svelte.ts
+++ src/lib/components/global/App.svelte.ts
@@ -1,3 +1,13 @@
+export const Breakpoints = {
+  '2xs': 384,
+  xs: 512,
+  sm: 640,
+  md: 768,
+  lg: 1024,
+  xl: 1280,
+  '2xl': 1536,
+};
+
 export const App = $state({
   viewportWidth: 0,
   viewportHeight: 0,
