b9d91e79efb0e6ed0fe490d29824ed378b243709
yoitsmedarls <johndaryljavier26@gmail.com>
2025-04-30 06:51:07 +0800

    Adding loading icons and created an Icon component.

---
+++ src/lib/components/global/Icon.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  let {
+    icon = '',
+    class: additionalStyles = '',
+  }: {
+    icon?: string | undefined;
+    class?: string | undefined;
+  } = $props();
+</script>
+
+{#if icon}
+  <span
+    class={[
+      'flex flex-col justify-center align-middle',
+      icon === 'progress_activity' ? 'animate-spin' : '',
+      additionalStyles ? additionalStyles : '',
+    ]}
+  >
+    <span class="material-symbols-rounded">
+      {icon}
+    </span>
+  </span>
+{/if}
