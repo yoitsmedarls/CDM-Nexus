928161b0b42d870f2208385fff4279709c29d6ff
yoitsmedarls <johndaryljavier26@gmail.com>
2025-05-04 20:30:12 +0800

    Fixed the same bug on the Calendar component.

--- src/lib/components/ui/Checkbox.svelte
+++ src/lib/components/ui/Checkbox.svelte
@@ -20,7 +20,7 @@
   <Checkbox.Root
     {id}
     aria-labelledby={id.concat('-label')}
-    class="peer inline-flex min-h-6 min-w-6 items-center justify-center rounded-md border-1 border-transparent bg-blue-950 text-white transition-all duration-100 ease-in-out not-disabled:focus-visible:outline-2 not-disabled:focus-visible:outline-offset-2 not-disabled:focus-visible:outline-blue-950/50 active:scale-[0.98] disabled:cursor-not-allowed disabled:border-gray-200 disabled:bg-white not-disabled:data-[state=unchecked]:border-blue-950/50 not-disabled:data-[state=unchecked]:bg-white not-disabled:data-[state=unchecked]:hover:border-blue-900 not-disabled:data-[state=unchecked]:active:border-blue-950"
+    class="peer inline-flex min-h-6 min-w-6 items-center justify-center rounded-md border-1 border-transparent bg-blue-950 text-white outline-transparent transition-colors duration-100 ease-in-out not-disabled:focus-visible:outline-2 not-disabled:focus-visible:outline-offset-2 not-disabled:focus-visible:outline-blue-950/50 active:scale-[0.98] disabled:cursor-not-allowed disabled:border-gray-200 disabled:bg-white not-disabled:data-[state=unchecked]:border-blue-950/50 not-disabled:data-[state=unchecked]:bg-white not-disabled:data-[state=unchecked]:hover:border-blue-900 not-disabled:data-[state=unchecked]:active:border-blue-950"
     bind:checked
     bind:ref
     {...restProps}
