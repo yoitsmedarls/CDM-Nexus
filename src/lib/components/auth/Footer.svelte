21ea8715b03d373b6e75a5f5f3eb323c94a8f8b2
yoitsmedarls <johndaryljavier26@gmail.com>
2025-05-04 21:44:00 +0800

    Branch reset...

--- src/lib/components/auth/Footer.svelte
+++
@@ -1,8 +0,0 @@
-<div class="pt-8 pb-4 md:absolute md:bottom-0 md:z-10">
-  <p
-    class="font-nunito text-2xs text-center text-gray-500 md:text-xs md:text-white md:opacity-30"
-  >
-    <!-- TODO: Add automatically updating year -->
-    &copy; CDM Nexus. All rights reserved.
-  </p>
-</div>
