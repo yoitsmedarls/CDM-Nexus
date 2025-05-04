c279e7a4969be47a6a041683c46e83b1c0706ad0
yoitsmedarls <johndaryljavier26@gmail.com>
2025-05-04 20:29:26 +0800

    Fixed a bug where the outline grows and shrinks on active toggle.

--- src/lib/components/ui/Calendar.svelte
+++ src/lib/components/ui/Calendar.svelte
@@ -52,7 +52,7 @@
         <select
           aria-label="Select month"
           value={placeholder.month}
-          class="font-poppins inline-flex w-full min-w-fit items-center justify-center rounded-md border-0 bg-white text-base font-medium text-gray-800 transition-all duration-100 open:bg-blue-100 hover:bg-blue-50 hover:text-blue-950 open:hover:bg-blue-100 focus-visible:ring-0 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-950/50 active:scale-[0.98] active:bg-blue-100 active:text-blue-950"
+          class="font-poppins inline-flex w-full min-w-fit items-center justify-center rounded-md border-0 bg-white text-base font-medium text-gray-800 outline-transparent transition-colors duration-100 open:bg-blue-100 hover:bg-blue-50 hover:text-blue-950 open:hover:bg-blue-100 focus-visible:ring-0 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-950/50 active:bg-blue-100 active:text-blue-950"
           onchange={(e) => {
             const month = parseInt(e.currentTarget.value);
             placeholder = placeholder.set({ month });
@@ -67,7 +67,7 @@
         <select
           aria-label="Select year"
           value={placeholder.year}
-          class="font-poppins inline-flex max-w-fit min-w-fit items-center justify-center rounded-md border-0 bg-white text-base font-medium text-gray-800 transition-all duration-100 open:bg-blue-100 hover:bg-blue-50 hover:text-blue-950 open:hover:bg-blue-100 focus-visible:ring-0 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-950/50 active:scale-[0.98] active:bg-blue-100 active:text-blue-950"
+          class="font-poppins inline-flex max-w-fit min-w-fit items-center justify-center rounded-md border-0 bg-white text-base font-medium text-gray-800 outline-transparent transition-colors duration-100 open:bg-blue-100 hover:bg-blue-50 hover:text-blue-950 open:hover:bg-blue-100 focus-visible:ring-0 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-950/50 active:bg-blue-100 active:text-blue-950"
           onchange={(e) => {
             const year = parseInt(e.currentTarget.value);
             placeholder = placeholder.set({ year });
@@ -84,12 +84,12 @@
           class="font-poppins w-full pl-2 text-left text-base font-medium text-balance text-gray-800"
         />
         <Calendar.PrevButton
-          class="inline-flex min-h-8 min-w-8 items-center justify-center rounded-md bg-white text-gray-800 transition-all duration-100 hover:bg-blue-50 hover:text-blue-950 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-800/50 active:scale-[0.98] active:bg-blue-100 active:text-blue-950"
+          class="inline-flex min-h-8 min-w-8 items-center justify-center rounded-md bg-white text-gray-800 outline-transparent transition-colors duration-100 hover:bg-blue-50 hover:text-blue-950 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-800/50 active:scale-[0.98] active:bg-blue-100 active:text-blue-950"
         >
           <CaretLeft class="size-5" />
         </Calendar.PrevButton>
         <Calendar.NextButton
-          class="inline-flex min-h-8 min-w-8 items-center justify-center rounded-md bg-white text-gray-800 transition-all duration-100 hover:bg-blue-50 hover:text-blue-950 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-800/50 active:scale-[0.98] active:bg-blue-100 active:text-blue-950"
+          class="inline-flex min-h-8 min-w-8 items-center justify-center rounded-md bg-white text-gray-800 outline-transparent transition-colors duration-100 hover:bg-blue-50 hover:text-blue-950 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-800/50 active:scale-[0.98] active:bg-blue-100 active:text-blue-950"
         >
           <CaretRight class="size-5" />
         </Calendar.NextButton>
@@ -119,7 +119,7 @@
                     class="relative size-10 text-center text-sm"
                   >
                     <Calendar.Day
-                      class="group font-nunito relative inline-flex size-10 flex-col items-center justify-center rounded-md bg-transparent p-0 text-sm font-normal whitespace-nowrap text-gray-800 transition-all duration-100 hover:bg-blue-50 hover:text-blue-950 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-950/50 active:bg-blue-100 active:text-blue-950 data-disabled:pointer-events-none data-disabled:text-gray-800/25 data-outside-month:pointer-events-none data-selected:bg-blue-950 data-selected:font-semibold data-selected:text-white data-unavailable:text-gray-800/50 data-unavailable:line-through data-unavailable:hover:bg-transparent"
+                      class="group font-nunito relative inline-flex size-10 flex-col items-center justify-center rounded-md bg-transparent p-0 text-sm font-normal whitespace-nowrap text-gray-800 outline-transparent transition-colors duration-100 hover:bg-blue-50 hover:text-blue-950 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-950/50 active:bg-blue-100 active:text-blue-950 data-disabled:pointer-events-none data-disabled:text-gray-800/25 data-outside-month:pointer-events-none data-selected:bg-blue-950 data-selected:font-semibold data-selected:text-white data-unavailable:text-gray-800/50 data-unavailable:line-through data-unavailable:hover:bg-transparent"
                     >
                       <div
                         class="absolute top-1 hidden size-1 rounded-full bg-blue-950 group-data-selected:bg-white group-data-today:block"
