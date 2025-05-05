06297ee49b3e350caff0f939a750e12218dc76af
yoitsmedarls <johndaryljavier26@gmail.com>
2025-05-06 01:47:52 +0800

    Added an input UI component.

---
+++ src/lib/components/ui/Input.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte';
+  import type { SvelteHTMLElements } from 'svelte/elements';
+
+  let {
+    children,
+    ...restProps
+  }: {
+    children: Snippet;
+  } & SvelteHTMLElements['input'] = $props();
+</script>
+
+<label class="block w-full min-w-fit">
+  <span class="font-poppins block py-1 text-sm text-gray-800 md:text-base">
+    {@render children()}
+  </span>
+  <input
+    class="font-nunito block w-full rounded-md border border-gray-200 p-2 pl-3 text-sm text-gray-800 outline-0 transition-all duration-100 placeholder:text-gray-800/50 hover:border-gray-800/50 focus:border-blue-800 focus:ring-blue-800 focus:placeholder:text-gray-800/75 md:text-base"
+    {...restProps}
+  />
+</label>
