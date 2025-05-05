a44e3bcd69be1a0a0089c96d93ce2dba142acb12
yoitsmedarls <johndaryljavier26@gmail.com>
2025-05-06 01:49:20 +0800

    Added a backdrop component (to be used in auth routes but can be used elsewhere).

---
+++ src/lib/components/ui/Backdrop.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { cubicInOut } from 'svelte/easing';
+  import type { SvelteHTMLElements } from 'svelte/elements';
+  import { fade } from 'svelte/transition';
+
+  let {
+    src,
+    duration = 100,
+    ...restProps
+  }: {
+    src: string;
+    duration?: number;
+  } & SvelteHTMLElements['div'] = $props();
+</script>
+
+<div
+  in:fade={{
+    duration: duration,
+    easing: cubicInOut,
+  }}
+  style={`background-image: url('${src}');`}
+  class="fixed flex h-full w-full grow flex-col bg-cover bg-scroll bg-center bg-no-repeat brightness-50 transition-all duration-100"
+  {...restProps}
+></div>
