dd7b4745edba034b8e589883f2c362353b6a9ef6
yoitsmedarls <johndaryljavier26@gmail.com>
2025-05-06 01:46:35 +0800

    Added a global app state.

---
+++ src/lib/components/global/App.svelte.ts
@@ -0,0 +1,21 @@
+const rem = 16;
+
+export const App = $state({
+  viewport: {
+    width: 0,
+    height: 0,
+  },
+  scroll: {
+    x: 0,
+    y: 0,
+  },
+  breakpoints: {
+    '2xs': 24 * rem,
+    xs: 32 * rem,
+    sm: 40 * rem,
+    md: 48 * rem,
+    lg: 64 * rem,
+    xl: 80 * rem,
+    '2xl': 96 * rem,
+  },
+});
