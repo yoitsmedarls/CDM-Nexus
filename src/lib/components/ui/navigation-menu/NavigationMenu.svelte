5aed60ba5edea98fb83d113eb8775fc38b22f12a
yoitsmedarls <johndaryljavier26@gmail.com>
2025-05-11 06:52:27 +0800

    Basic version of the navigation menu component. To be improved.

---
+++ src/lib/components/ui/navigation-menu/NavigationMenu.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { NavigationMenu } from 'bits-ui';
+  import type { Snippet } from 'svelte';
+
+  let {
+    menuDuration = 100,
+    children,
+  }: {
+    menuDuration?: number;
+    children: Snippet;
+  } = $props();
+</script>
+
+<NavigationMenu.Root
+  class="flex h-fit w-full grow flex-row justify-end align-middle"
+>
+  <NavigationMenu.List
+    class="flex h-full list-none flex-row items-center justify-center gap-2 px-2"
+  >
+    {@render children()}
+  </NavigationMenu.List>
+</NavigationMenu.Root>
