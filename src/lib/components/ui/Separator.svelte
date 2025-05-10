15dd94661547689194cc9da9308ca128691086b9
yoitsmedarls <johndaryljavier26@gmail.com>
2025-05-11 05:48:04 +0800

    Updated the separator component to support vertical orientation.

--- src/lib/components/ui/Separator.svelte
+++ src/lib/components/ui/Separator.svelte
@@ -1,5 +1,18 @@
 <script lang="ts">
-  import { Separator } from 'bits-ui';
+  import { Separator, type WithoutChildrenOrChild } from 'bits-ui';
+
+  let {
+    class: additionalStyles = 'bg-black/5',
+    ...restProps
+  }: WithoutChildrenOrChild<Separator.RootProps> = $props();
 </script>
 
-<Separator.Root class="my-1 h-[0.5px] w-full bg-black/5" />
+<Separator.Root
+  class={[
+    restProps.orientation === 'horizontal'
+      ? 'my-1 h-px w-full'
+      : 'mx-1 h-full w-px',
+    additionalStyles,
+  ]}
+  {...restProps}
+/>
