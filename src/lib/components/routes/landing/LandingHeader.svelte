852ddb9f46a4c15306d20217f770702da0a50c15
yoitsmedarls <johndaryljavier26@gmail.com>
2025-05-11 05:47:44 +0800

    Initial versions of the landing header and section wrappers.

---
+++ src/lib/components/routes/landing/LandingHeader.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte';
+  import type { SvelteHTMLElements } from 'svelte/elements';
+
+  let {
+    children,
+    ...restProps
+  }: {
+    children: Snippet;
+  } & SvelteHTMLElements['div'] = $props();
+</script>
+
+<div
+  class="flex h-16 w-full flex-row items-center justify-between gap-2 px-3 transition-all duration-100 md:h-20"
+  {...restProps}
+>
+  {@render children()}
+</div>
---
+++ src/lib/components/routes/landing/LandingSection.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte';
+  import type { ClassValue, SvelteHTMLElements } from 'svelte/elements';
+
+  let {
+    class: additionalStyles,
+    children,
+    ...restProps
+  }: {
+    class: ClassValue | undefined;
+    children: Snippet;
+  } & SvelteHTMLElements['section'] = $props();
+</script>
+
+<section class={['flex min-h-fit flex-col']} {...restProps}>
+  {#if children}
+    <div class={['mx-auto flex w-full grow flex-col', additionalStyles]}>
+      {@render children()}
+    </div>
+  {/if}
+</section>
