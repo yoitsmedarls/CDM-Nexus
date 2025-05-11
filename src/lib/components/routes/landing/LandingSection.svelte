6fd5c51b107819e70f74670a48f9d822fe208ae0
yoitsmedarls <johndaryljavier26@gmail.com>
2025-05-12 07:01:17 +0800

    Added separate styling options for the landing section elements.

--- src/lib/components/routes/landing/LandingSection.svelte
+++ src/lib/components/routes/landing/LandingSection.svelte
@@ -7,14 +7,22 @@
     children,
     ...restProps
   }: {
-    class: ClassValue | undefined;
+    class?:
+      | {
+          section?: ClassValue | null | undefined;
+          div?: ClassValue | null | undefined;
+        }
+      | undefined;
     children: Snippet;
   } & SvelteHTMLElements['section'] = $props();
 </script>
 
-<section class={['flex min-h-fit flex-col']} {...restProps}>
+<section
+  class={['flex min-h-fit flex-col', additionalStyles?.section]}
+  {...restProps}
+>
   {#if children}
-    <div class={['mx-auto flex w-full grow flex-col', additionalStyles]}>
+    <div class={['mx-auto flex w-full grow flex-col', additionalStyles?.div]}>
       {@render children()}
     </div>
   {/if}
