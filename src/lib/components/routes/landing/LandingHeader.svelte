f670aec73169af38b0d08ac87a05e42f855527b4
yoitsmedarls <johndaryljavier26@gmail.com>
2025-05-12 07:00:39 +0800

    Made the landing page header persist on scroll.

--- src/lib/components/routes/landing/LandingHeader.svelte
+++ src/lib/components/routes/landing/LandingHeader.svelte
@@ -2,17 +2,41 @@
   import type { Snippet } from 'svelte';
   import type { SvelteHTMLElements } from 'svelte/elements';
 
+  import { fade } from 'svelte/transition';
+
   let {
+    duration = 100,
+    scrollValue,
+    scrollTrigger,
     children,
     ...restProps
   }: {
+    duration?: number;
+    scrollValue: number;
+    scrollTrigger: number;
     children: Snippet;
   } & SvelteHTMLElements['div'] = $props();
 </script>
 
+{#if scrollValue > scrollTrigger}
+  <div
+    transition:fade={{ duration: duration }}
+    class="fixed top-0 right-0 left-0 z-50 flex h-14 w-full flex-row justify-center bg-white drop-shadow-sm transition-all duration-100 md:h-16"
+  >
+    <div
+      class="mx-auto flex h-full w-full max-w-[88rem] flex-row items-center justify-between gap-2 px-3 transition-all duration-100"
+      {...restProps}
+    >
+      {@render children()}
+    </div>
+  </div>
+{/if}
+
 <div
-  class="flex h-16 w-full flex-row items-center justify-between gap-2 px-3 transition-all duration-100 md:h-18"
+  class="flex h-14 w-full flex-row items-center justify-between gap-2 bg-transparent px-3 transition-all duration-100 md:h-16"
   {...restProps}
 >
-  {@render children()}
+  {#if scrollValue <= scrollTrigger}
+    {@render children()}
+  {/if}
 </div>
