e22e033dc7958bede0b16c327789605db8c65770
yoitsmedarls <johndaryljavier26@gmail.com>
2025-05-14 11:00:56 +0800

    Added support for sub menus with the navigation menu dropdown component.

--- src/lib/components/ui/navigation-menu/NavigationMenu.svelte
+++ src/lib/components/ui/navigation-menu/NavigationMenu.svelte
@@ -21,12 +21,19 @@
   }}
 >
   <NavigationMenu.Root
-    class="flex h-fit w-full grow flex-row justify-end align-middle"
+    class="relative flex h-fit w-full grow flex-row justify-end align-middle"
   >
     <NavigationMenu.List
       class="flex h-full list-none flex-row items-center justify-center gap-2 px-2"
     >
       {@render children()}
     </NavigationMenu.List>
+
+    <div class="absolute top-full left-0 z-50 flex h-max w-full justify-start">
+      <NavigationMenu.Viewport
+        forceMount
+        class="data-[state=closed]:animate-fade-out data-[state=open]:animate-fade-in relative mt-2 h-max w-max origin-top-right overflow-scroll rounded-md border border-gray-200 bg-white text-gray-800 drop-shadow-md transition-[width,_height] duration-200 data-[state=closed]:hidden"
+      />
+    </div>
   </NavigationMenu.Root>
 </div>
