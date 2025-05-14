a0da103707dff8c5ff8d7586771644d8b6671510
yoitsmedarls <johndaryljavier26@gmail.com>
2025-05-14 11:02:57 +0800

    Adjusted the bottom positioning.

--- src/lib/components/ui/ScrollToTop.svelte
+++ src/lib/components/ui/ScrollToTop.svelte
@@ -13,7 +13,7 @@
 
 <button
   transition:fade={{ duration: duration }}
-  class="font-poppins fixed right-4 bottom-8 z-100 inline-flex max-h-fit min-h-fit max-w-fit min-w-fit flex-col justify-center overflow-clip rounded-full border-0 bg-white p-2 text-center align-middle font-medium text-gray-800 outline-transparent drop-shadow-xl transition-colors duration-100 hover:cursor-pointer not-disabled:hover:bg-blue-50 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-800/50 active:scale-[0.98] not-disabled:active:bg-blue-100 not-disabled:active:text-blue-950 disabled:cursor-not-allowed disabled:opacity-50"
+  class="font-poppins fixed right-4 bottom-6 z-100 inline-flex max-h-fit min-h-fit max-w-fit min-w-fit flex-col justify-center overflow-clip rounded-full border-0 bg-white p-2 text-center align-middle font-medium text-gray-800 outline-transparent drop-shadow-xl transition-colors duration-100 hover:cursor-pointer not-disabled:hover:bg-blue-50 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-800/50 active:scale-[0.98] not-disabled:active:bg-blue-100 not-disabled:active:text-blue-950 disabled:cursor-not-allowed disabled:opacity-50"
   {...restProps}
 >
   <ArrowUp class="size-6" weight="bold" />
