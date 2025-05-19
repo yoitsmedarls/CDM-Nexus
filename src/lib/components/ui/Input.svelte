3e177c639932565de531ba50e43ae57c7b253647
yoitsmedarls <johndaryljavier26@gmail.com>
2025-05-19 13:28:45 +0800

    Adjusted the padding and line height.

--- src/lib/components/ui/Input.svelte
+++ src/lib/components/ui/Input.svelte
@@ -15,7 +15,7 @@
     {@render children()}
   </span>
   <input
-    class="font-nunito block w-full rounded-md border border-gray-200 p-2 pl-3 text-sm text-gray-800 outline-0 transition-all duration-100 placeholder:text-gray-800/50 hover:border-gray-800/50 focus:border-blue-800 focus:ring-blue-800 focus:placeholder:text-gray-800/75 md:text-base"
+    class="font-nunito block w-full rounded-md border border-gray-200 p-2 pl-2.5 align-middle text-sm leading-tight text-gray-800 outline-0 transition-all duration-100 placeholder:text-gray-800/50 hover:border-gray-800/50 focus:border-blue-800 focus:ring-blue-800 focus:placeholder:text-gray-800/60 md:text-base"
     {...restProps}
   />
 </label>
