eba370392f94fe68056477431df38e077994ca31
yoitsmedarls <johndaryljavier26@gmail.com>
2025-05-19 13:23:13 +0800

    Made the auth window more contextualized by showing a message if the user was redirected to the auth pages.

--- src/lib/components/routes/auth/AuthWindow.svelte
+++ src/lib/components/routes/auth/AuthWindow.svelte
@@ -6,24 +6,31 @@
   let {
     title,
     duration = 100,
+    searchParams,
     children,
     ...restProps
   }: {
     title: string;
     duration?: number;
+    searchParams: URLSearchParams;
     children: Snippet;
   } & SvelteHTMLElements['main'] = $props();
 </script>
 
 <main
   in:fade={{ duration: duration }}
-  class="mx-auto flex w-full max-w-2xl grow flex-col justify-start gap-4 bg-transparent px-4 pt-8 pb-6 transition-all duration-100 max-md:max-w-lg md:absolute md:z-20 md:rounded-xl md:bg-white md:px-6 md:pt-10 md:drop-shadow-2xl"
+  class="mx-auto flex w-full max-w-xl grow flex-col justify-start gap-4 bg-transparent px-4 pt-8 pb-6 transition-all duration-100 max-md:max-w-lg md:absolute md:z-20 md:rounded-xl md:bg-white md:px-6 md:pt-10 md:drop-shadow-2xl"
   {...restProps}
 >
   <h1
     class="font-poppins text-xl font-semibold whitespace-nowrap text-gray-800 md:text-2xl"
   >
     {title}
+    {#if searchParams.get('redirectTo')}
+      <p class="font-poppins pt-2 text-sm font-normal text-gray-800/75 italic">
+        To continue, please {title.toLowerCase()}:
+      </p>
+    {/if}
   </h1>
   {@render children()}
 </main>
