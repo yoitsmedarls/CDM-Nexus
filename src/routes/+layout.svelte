bc2ef3ddc4041248176adaabc30d09006cbbf1a3
yoitsmedarls <johndaryljavier26@gmail.com>
2025-05-20 21:14:13 +0800

    Integrated the addition of minimum height and width to the viewport warning, and added more comments.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,29 @@
 <script lang="ts">
+  // Imports the main stylesheet for the web app, powered by Tailwind CSS.
   import '../app.css';
 
+  // Type imports
   import type { LayoutProps } from './$types';
 
+  // Global app state object
   import { App } from '$lib/components/global/App.svelte';
 
+  // SvelteKit's built-in app state
   import { navigating } from '$app/state';
+
+  // Svelte transitions
   import { fade } from 'svelte/transition';
   import { cubicInOut } from 'svelte/easing';
 
+  // Route-specific components
   import LoadingOverlay from '$lib/components/routes/LoadingOverlay.svelte';
   import ViewportWarning from '$lib/components/routes/ViewportWarning.svelte';
 
+  // Layout props
   let { children }: LayoutProps = $props();
 </script>
 
-<!-- Window bindings for our global app state object -->
+<!-- Window bindings for the global app state object -->
 <svelte:window
   bind:innerWidth={App.viewport.width}
   bind:innerHeight={App.viewport.height}
@@ -33,7 +41,7 @@
   <!-- This causes weird layout shifts in the UI on the initial page load. -->
   <!-- As such, this condition shows a loading indicator to allow the global app state to load first before showing anything to the end user. -->
   <LoadingOverlay duration={200} />
-{:else if App.viewport.width < App.breakpoints.min}
+{:else if App.viewport.width < App.breakpoints.minw || App.viewport.height < App.breakpoints.minh}
   <!-- This condition then handles the cases where the viewport is too narrow after loading the page, instructing the user to resize their window. -->
   <ViewportWarning duration={200} />
 {:else}
