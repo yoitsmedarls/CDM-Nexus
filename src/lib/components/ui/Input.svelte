11e09984fef793da8fcefc5214558aebb674dacf
yoitsmedarls <johndaryljavier26@gmail.com>
2025-05-20 17:33:46 +0800

    Added styling for disabled states and removed the responsive text sizing.

--- src/lib/components/ui/Input.svelte
+++ src/lib/components/ui/Input.svelte
@@ -11,11 +11,11 @@
 </script>
 
 <label class="block w-full min-w-fit">
-  <span class="font-poppins block py-1 text-sm text-gray-800 md:text-base">
+  <span class="font-poppins block py-1 text-sm text-gray-800">
     {@render children()}
   </span>
   <input
-    class="font-nunito block w-full rounded-md border border-gray-200 p-2 pl-2.5 align-middle text-sm leading-tight text-gray-800 outline-0 transition-all duration-100 placeholder:text-gray-800/50 hover:border-gray-800/50 focus:border-blue-800 focus:ring-blue-800 focus:placeholder:text-gray-800/60 md:text-base"
+    class="font-nunito block w-full rounded-md border border-gray-200 p-2 pl-2.5 align-middle text-sm leading-tight text-gray-800 outline-0 transition-all duration-100 placeholder:text-gray-800/50 hover:border-gray-800/50 focus:border-blue-800 focus:ring-blue-800 focus:placeholder:text-gray-800/60 disabled:cursor-not-allowed disabled:opacity-50"
     {...restProps}
   />
 </label>
