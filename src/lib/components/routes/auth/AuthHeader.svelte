0c7c631aee73f44c1cf5844b81e105f4cd717c41
yoitsmedarls <johndaryljavier26@gmail.com>
2025-05-20 09:19:45 +0800

    Uses padding to set height instead of a min-height property.

--- src/lib/components/routes/auth/AuthHeader.svelte
+++ src/lib/components/routes/auth/AuthHeader.svelte
@@ -11,7 +11,7 @@
 </script>
 
 <header
-  class="flex min-h-20 w-full flex-row items-center justify-center transition-all duration-100 md:fixed md:top-0 md:right-0 md:left-0 md:px-6"
+  class="flex w-full flex-row items-center justify-center py-6 transition-all duration-100 md:fixed md:top-0 md:right-0 md:left-0 md:px-6"
   {...restProps}
 >
   {@render children()}
