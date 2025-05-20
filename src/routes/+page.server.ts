77e311aa45a05a05eebc73399994c1ed97c5a2c0
yoitsmedarls <johndaryljavier26@gmail.com>
2025-05-20 21:13:35 +0800

    Added comments to the server load of the landing page.

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,6 +2,7 @@ import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ locals }) => {
   return {
+    // Returns the currently logged in user's role for the proper color coding of the Avatar component in the landing page.
     user: locals.user
       ? {
           role: locals.user.role,
