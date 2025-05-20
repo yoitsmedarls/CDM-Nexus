82642941c5d1f660ee4596a3a5426c436586dc3c
yoitsmedarls <johndaryljavier26@gmail.com>
2025-05-20 21:06:15 +0800

    Switched the application panel to be a centered floating window that is y-scrollable.

--- src/lib/components/routes/apply/ApplicationPanel.svelte
+++ src/lib/components/routes/apply/ApplicationPanel.svelte
@@ -15,7 +15,7 @@
 
 <main
   in:fade={{ duration: duration }}
-  class="flex w-full max-w-lg grow flex-col justify-start bg-white px-4 transition-all duration-100 md:fixed md:top-0 md:right-0 md:bottom-0 md:max-w-sm md:bg-white md:px-6 md:drop-shadow-xl"
+  class="flex w-full max-w-lg grow flex-col justify-start bg-white px-4 transition-all duration-100 md:fixed md:max-h-[calc(100%-2rem)] md:self-center md:overflow-y-auto md:rounded-lg md:bg-white md:px-6 md:drop-shadow-2xl"
   {...restProps}
 >
   {@render children()}
