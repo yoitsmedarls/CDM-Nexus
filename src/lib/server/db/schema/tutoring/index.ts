a19e9fbf6191483036cd9fde752b88f8d4fb9bb0
yoitsmedarls <johndaryljavier26@gmail.com>
2025-05-22 21:47:58 +0800

    Removed the imports of the deleted tables.

--- src/lib/server/db/schema/tutoring/index.ts
+++ src/lib/server/db/schema/tutoring/index.ts
@@ -1,10 +1,5 @@
 // Enums
 export * from './applicationStatusEnum';
-export * from './availabilityEnum';
-export * from './eventCategoryEnum';
 
 // Tables
 export * from './applications';
-export * from './events';
-export * from './schedules';
-export * from './timeslots';
